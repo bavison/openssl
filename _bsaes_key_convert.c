3b703e04036a8ed18a237df2393e16f41c28b7cd
Ben Avison <bavison@riscosopen.org>
2021-01-18 17:17:44 +0000

    Fuzz now workks on AArch32

--- _bsaes_key_convert.c
+++ _bsaes_key_convert.c
@@ -2,13 +2,27 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <inttypes.h>
+
+#define STR(x) #x
 
 const unsigned int default_number_of_runs;
 void benchmark(void);
-void fuzz(void);
+void fuzz(unsigned int seed);
+
+/** Storage for core registers:
+ * AArch32: r0-r12         32-bit long = 32-bit registers
+ * AArch64: x0-x3, x9-x17  64-bit long = 64-bit registers
+ */
+static long r[13];
+/** Storage for NEON registers:
+ *  q0-q15  16 x 16-byte registers
+ *  AArch64's q16-q31 not considered since no analogy in AArch32
+ */
+static uint8_t v[256];
 
 /** Produce random number with 32 bits of entropy */
-inline uint32_t rand32(void)
+static inline uint32_t rand32(void)
 {
     /* rand() is technically only required to produce 15 bits of entropy by the
      * C standard, but in practice most implementations produce 31 bits */
@@ -16,15 +30,21 @@ inline uint32_t rand32(void)
 }
 
 /** Produce random number with 64 bits of entropy */
-inline uint64_t rand64(void)
+static inline uint64_t rand64(void)
 {
     /* rand() is technically only required to produce 15 bits of entropy by the
      * C standard, but in practice most implementations produce 31 bits */
     return (uint64_t) rand() << 48 ^ (uint64_t) rand() << 32 ^ (uint64_t) rand() << 16 ^ rand();
 }
 
+/** Produce random number within limmits */
+static inline uint32_t rand_limited(uint32_t min, uint32_t max)
+{
+    return min + rand32() % (max + 1 - min);
+}
+
 /** Fill a buffer with random numbers */
-inline void rand_buffer(void *b, size_t s)
+static inline void rand_buffer(void *b, size_t s)
 {
     /* rand() is technically only required to produce 15 bits of entropy by the
      * C standard, but in practice most implementations produce 31 bits */
@@ -35,40 +55,50 @@ inline void rand_buffer(void *b, size_t s)
         *(uint8_t *)p = rand();
 }
 
-int main(int argc, char *argv[])
+/** Initialise all registers randomly, to ensure we don't
+ *  depend on their values. Generate 64 bits of randomness
+ *  for core registers, even in 32-bit mode, to ensure the
+ *  RNG remains in step with 64-bit mode.
+ */
+static void randomise_registers(void)
 {
-    if ((argc > 1 && strcmp(argv[1], "--help") == 0) || argc >= 4)
-    {
-        fprintf(stderr, "Syntax:\n"
-                        "%s benchmark\n"
-                        "or\n"
-                        "%s\n"
-                        "or\n"
-                        "%s <fuzz seed>\n"
-                        "or\n"
-                        "%s <fuzz seed minimum> <fuzz seed maximum>\n",
-                        argv[0], argv[0], argv[0], argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    if (argc == 2 && strcmp(argv[1], "benchmark") == 0)
-    {
-        benchmark();
-        exit(EXIT_SUCCESS);
-    }
-    unsigned int seed_min = argc >= 2 ? strtoul(argv[1], NULL, 10) : 0;
-    unsigned int seed_max = argc >= 3 ? strtoul(argv[2], NULL, 10) + 1 :
-                            argc == 2 ? strtoul(argv[1], NULL, 10) + 1 :
-                            default_number_of_runs;
-    for (unsigned int seed = seed_min; seed < seed_max; ++seed)
-    {
-        srand(seed);
-    }
-    exit(EXIT_SUCCESS);
+    for (size_t i = 0; i < 13; ++i)
+        r[i] = rand64();
+    rand_buffer(v, sizeof v);
 }
 
+#define DUMP8(x)  do { printf("%u: " STR(x) ": %02"  PRIX8  "\n", seed, x); } while (0)
+#define DUMP16(x) do { printf("%u: " STR(x) ": %04"  PRIX16 "\n", seed, x); } while (0)
+#define DUMP32(x) do { printf("%u: " STR(x) ": %08"  PRIX32 "\n", seed, x); } while (0)
+#define DUMP64(x) do { printf("%u: " STR(x) ": %016" PRIX64 "\n", seed, x); } while (0)
+#define DUMP_OFFSET(p, base) do { printf("%u: " STR(x) ": " STR(base) "+%zd\n", seed, (uint8_t *) p - (uint8_t *) base); } while (0)
+#define DUMP_ARRAY(b) do { dump_buffer(seed, STR(b), b, sizeof b); } while (0)
 
-const unsigned int default_number_of_runs = 1;
+static void dump_buffer(unsigned int seed, const char *name, const void *b, size_t s)
+{
+    const uint8_t *bb = b;
+    size_t offset = 0;
+    printf("%u: %s:\n", seed, name);
+    while (offset < s)
+    {
+        if ((offset & 15) == 0)
+            printf("%08zX :", offset);
+        if (offset + 4 <= s)
+        {
+            printf(" %08X", *(uint32_t *)(bb + offset));
+            offset += 4;
+        }
+        else if ((offset + 1 <= s))
+        {
+            printf(" %02X", bb[offset]);
+            offset += 1;
+        }
+        if ((offset & 15) == 0 || offset == s)
+            printf("\n");
+    }
+}
 
+/** Facilitate testing of functions with non-standard argument/result marshalling */
 static void veneer(void (*routine)(), long r[13], uint8_t v[256])
 {
     __asm__ volatile (
@@ -191,28 +221,52 @@ static void veneer(void (*routine)(), long r[13], uint8_t v[256])
     );
 }
 
-static long r[13];
-static uint8_t v[256];
-
-static void randomise_registers(void)
+int main(int argc, char *argv[])
 {
-    /* Initialise all registers randomly, to ensure we don't
-     * depend on their values. Generate 64 bits of randomness
-     * for core registers, even in 32-bit mode, to ensure the
-     * RNG remains in step with 64-bit mode.
-     */
-    for (size_t i = 0; i < 13; ++i)
-        r[i] = rand64();
-    rand_buffer(v, sizeof v);
+    if ((argc > 1 && strcmp(argv[1], "--help") == 0) || argc >= 4)
+    {
+        fprintf(stderr, "Syntax:\n"
+                        "%s benchmark\n"
+                        "or\n"
+                        "%s\n"
+                        "or\n"
+                        "%s <fuzz seed>\n"
+                        "or\n"
+                        "%s <fuzz seed minimum> <fuzz seed maximum>\n",
+                        argv[0], argv[0], argv[0], argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    if (argc == 2 && strcmp(argv[1], "benchmark") == 0)
+    {
+        benchmark();
+        exit(EXIT_SUCCESS);
+    }
+    unsigned int seed_min = argc >= 2 ? strtoul(argv[1], NULL, 10) : 0;
+    unsigned int seed_max = argc >= 3 ? strtoul(argv[2], NULL, 10) + 1 :
+                            argc == 2 ? strtoul(argv[1], NULL, 10) + 1 :
+                            default_number_of_runs;
+    for (unsigned int seed = seed_min; seed < seed_max; ++seed)
+    {
+        srand(seed);
+        fuzz(seed);
+    }
+    exit(EXIT_SUCCESS);
 }
+
+
+
+#define MIN_ROUNDS 2
+#define MAX_ROUNDS 16
 
 extern void _bsaes_key_convert(void);
 
-static void wrapper(void (*routine)(), void *input_key, long counter, void *output_key, void **lm_pointer, void **updated_output_key, uint32_t (*pattern)[4], uint32_t (*last_round_key)[4])
+const unsigned int default_number_of_runs = 100;
+
+static void wrapper(void (*routine)(), void *input_key, long rounds, void *output_key, void **lm_pointer, void **updated_output_key, uint32_t (*pattern)[4], uint32_t (*last_round_key)[4])
 {
     /* Marshall arguments into the appropriate places */
     r[4] = (long) input_key;
-    r[5] = counter;
+    r[5] = rounds;
     r[12] = (long) output_key;
 
     /* Call generic assembly veneer */
@@ -229,10 +283,11 @@ void benchmark(void)
 {
 }
 
-void fuzz(void)
+void fuzz(unsigned int seed)
 {
-    uint8_t input_key[1024];
-    uint8_t output_key[1024];
+    long rounds = rand_limited(MIN_ROUNDS, MAX_ROUNDS);
+    uint8_t input_key[16*(rounds+1)];
+    uint8_t output_key[128*rounds - 96];
     uint32_t pattern[4];
     uint32_t last_round_key[4];
     uint32_t *lm_pointer;
@@ -241,6 +296,13 @@ void fuzz(void)
     rand_buffer(input_key, sizeof input_key);
     rand_buffer(output_key, sizeof output_key);
     randomise_registers();
-    wrapper(_bsaes_key_convert, input_key, 1, output_key, (void **) &lm_pointer, (void **) &updated_output_key, &pattern, &last_round_key);
+
+    wrapper(_bsaes_key_convert, input_key, rounds, output_key, (void **) &lm_pointer, (void **) &updated_output_key, &pattern, &last_round_key);
+
+    DUMP_ARRAY(output_key);
+    DUMP_ARRAY(pattern);
+    DUMP_ARRAY(last_round_key);
+    DUMP32(*lm_pointer);
+    DUMP_OFFSET(updated_output_key, output_key);
 }
 
--- build-test-aarch32.sh
+++ build-test-aarch32.sh
@@ -2,8 +2,8 @@
 
 set -e
 rsync -av 192.168.0.172:openssl/_bsaes_key_convert.c .
-rsync -av 192.168.0.172:openssl/build-test-aarch32.c .
-rsync -av 192.168.0.172:openssl/build-test-aarch64.c .
+rsync -av 192.168.0.172:openssl/build-test-aarch32.sh .
+rsync -av 192.168.0.172:openssl/build-test-aarch64.sh .
 gcc -c -mfpu=neon-vfpv4 -g -Wall -Wextra -O2 -o _bsaes_key_convert.o _bsaes_key_convert.c
-gcc -o _bsaes_key_convert _bsaes_key_convert.o crypto/aes/bsaes-armv7.o crypto/aes/aes_cbc.o crypto/modes/cbc128.o crypto/aes/aes-armv4.o
+gcc -g -o _bsaes_key_convert _bsaes_key_convert.o crypto/aes/bsaes-armv7.o crypto/aes/aes_cbc.o crypto/modes/cbc128.o crypto/aes/aes-armv4.o
 objdump -d _bsaes_key_convert.o

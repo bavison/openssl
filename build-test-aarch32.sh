20b13fda3a433aa79171da82642d92890384dbdd
Ben Avison <bavison@riscosopen.org>
2021-02-08 16:11:26 +0000

    Implement AArch64 version of _bsaes_encrypt8

--- build-test-aarch32.sh
+++ build-test-aarch32.sh
@@ -2,6 +2,7 @@
 
 set -e
 rsync -av 192.168.0.172:openssl/_bsaes_decrypt8.c .
+rsync -av 192.168.0.172:openssl/_bsaes_encrypt8.c .
 rsync -av 192.168.0.172:openssl/_bsaes_key_convert.c .
 rsync -av 192.168.0.172:openssl/tester.h .
 rsync -av 192.168.0.172:openssl/build-test-aarch32.sh .
@@ -13,5 +14,9 @@ gcc -g -o _bsaes_key_convert _bsaes_key_convert.o crypto/aes/bsaes-armv7.o crypt
 gcc -c -mfpu=neon-vfpv4 -g -Wall -Wextra -O2 -o _bsaes_decrypt8.o _bsaes_decrypt8.c
 gcc -g -o _bsaes_decrypt8 _bsaes_decrypt8.o crypto/aes/bsaes-armv7.o crypto/aes/aes_cbc.o crypto/modes/cbc128.o crypto/aes/aes-armv4.o
 
+gcc -c -mfpu=neon-vfpv4 -g -Wall -Wextra -O2 -o _bsaes_encrypt8.o _bsaes_encrypt8.c
+gcc -g -o _bsaes_encrypt8 _bsaes_encrypt8.o crypto/aes/bsaes-armv7.o crypto/aes/aes_cbc.o crypto/modes/cbc128.o crypto/aes/aes-armv4.o
+
 #objdump -d _bsaes_key_convert.o
-objdump -d _bsaes_decrypt8.o
+#objdump -d _bsaes_decrypt8.o
+objdump -d _bsaes_encrypt8.o
--- build-test-aarch64.sh
+++ build-test-aarch64.sh
@@ -8,5 +8,9 @@ gcc -g -o _bsaes_key_convert _bsaes_key_convert.o crypto/aes/bsaes-armv8.o crypt
 gcc -c -g -Wall -Wextra -O2 -o _bsaes_decrypt8.o _bsaes_decrypt8.c
 gcc -g -o _bsaes_decrypt8 _bsaes_decrypt8.o crypto/aes/bsaes-armv8.o crypto/aes/aes_cbc.o crypto/modes/cbc128.o crypto/aes/aes_core.o
 
+gcc -c -g -Wall -Wextra -O2 -o _bsaes_encrypt8.o _bsaes_encrypt8.c
+gcc -g -o _bsaes_encrypt8 _bsaes_encrypt8.o crypto/aes/bsaes-armv8.o crypto/aes/aes_cbc.o crypto/modes/cbc128.o crypto/aes/aes_core.o
+
 #objdump -d _bsaes_key_convert.o
-objdump -d _bsaes_decrypt8.o
+#objdump -d _bsaes_decrypt8.o
+objdump -d _bsaes_encrypt8.o

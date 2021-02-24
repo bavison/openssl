7274126c195bccca71ac671099a25230360d599d
Ben Avison <bavison@riscosopen.org>
2021-02-24 16:24:49 +0000

    Implement AArch64 version of bsaes_xts_decrypt

--- build-test-aarch64.sh
+++ build-test-aarch64.sh
@@ -17,8 +17,12 @@ gcc -g -o bsaes_ctr32_encrypt_blocks bsaes_ctr32_encrypt_blocks.o crypto/aes/bsa
 gcc -c -g -Wall -Wextra -O2 -o bsaes_cbc_encrypt.o bsaes_cbc_encrypt.c
 gcc -g -o bsaes_cbc_encrypt bsaes_cbc_encrypt.o crypto/aes/bsaes-armv8.o crypto/aes/aes_cbc.o crypto/modes/cbc128.o crypto/aes/aes_core.o
 
+gcc -c -g -Wall -Wextra -O2 -o bsaes_xts_decrypt.o bsaes_xts_decrypt.c
+gcc -g -o bsaes_xts_decrypt bsaes_xts_decrypt.o crypto/aes/bsaes-armv8.o crypto/aes/aes_cbc.o crypto/modes/cbc128.o crypto/aes/aes_core.o
+
 #objdump -d _bsaes_key_convert.o
 #objdump -d _bsaes_decrypt8.o
 #objdump -d _bsaes_encrypt8.o
 #objdump -d bsaes_ctr32_encrypt_blocks.o
-objdump -d bsaes_cbc_encrypt.o
+#objdump -d bsaes_cbc_encrypt.o
+objdump -d bsaes_xts_decrypt.o
